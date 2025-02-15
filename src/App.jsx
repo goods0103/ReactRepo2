66fb6a74c0c86d07f0d99c31d424856d7659e1e0
윤제영 <goods0103@naver.com>
2025-02-15 22:32:09 +0900

    main-page commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
-      
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className='main-bg'></div>
+      <Row>
+        <Col sm>
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+          <h4>상품명</h4>
+          <p>상품정보</p>
+        </Col>
+        <Col sm>
+          <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+          <h4>상품명</h4>
+          <p>상품정보</p>
+        </Col>
+        <Col sm>
+          <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+          <h4>상품명</h4>
+          <p>상품정보</p>
+        </Col>
+      </Row>
     </div>
   )
 }
