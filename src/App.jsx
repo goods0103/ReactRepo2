e413637d7339497f8dbfa8c32185b00e23fb963e
윤제영 <p.jyjy301@kalmate.net>
2025-02-18 16:47:46 +0900

    폴더구조 변경

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,45 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
 import data from './data.js';
+import Detail from './Routes/Detail.jsx'
+import { Routes, Route, Link } from 'react-router-dom'
 function App() {
   const [shoes] = useState(data)
   return (
     <div className='App'>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <div className='main-bg'></div>
-      {/* 컴포넌트화하기  */}
-      <Row>
-        {
-          shoes.map((a, i) => {
-            return (
-              <Col sm>
-                <ProductComp shoes={shoes[i]} let i={i + 1} />
-              </Col>
-            )
-          })
-        }
-      </Row>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div>main
+              <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                  <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+                  <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                  </Nav>
+                </Container>
+              </Navbar>
+              <div className='main-bg'></div>
+              <Link to="/detail">상세페이지</Link>
+              <Link to="/about">about</Link>
+              <Row>
+                {
+                  shoes.map((a, i) => {
+                    return (
+                      <Col sm>
+                        <ProductComp shoes={shoes[i]} let i={i + 1} />
+                      </Col>
+                    )
+                  })
+                }
+              </Row>
+            </div>
+          </>} />
+        <Route path="/detail" element={<Detail/>} />
+        <Route path="/about" element={<div>about</div>} />
+      </Routes>
     </div>
   )
 }
@@ -42,4 +54,5 @@ const ProductComp = (props) => {
     </>
   )
 }
+
 export default App
