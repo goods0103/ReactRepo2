fece88226e595d1500536766d9daf17c70840e42
윤제영 <goods0103@naver.com>
2025-02-18 23:48:03 +0900

    styled components

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,11 +1,18 @@
 import { useParams } from "react-router-dom"
+import styled from "styled-components";
+
+let YellowButton = styled.button`
+  background : ${ props => props.bg};
+  color : ${ props => props.bg == 'blue' ? 'white' : 'black'};
+  padding : 10px;
+`;
 
 const Detail = (props) => {
     let {id} = useParams();
     let copy = props.shoes.find((v) => v.id == id)
-    console.log()
     return (
       <div className="container">
+        <YellowButton bg="yellow">button</YellowButton>
         <div className="row">
           <div className="col-md-6">
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
